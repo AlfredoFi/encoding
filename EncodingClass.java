6b4e53b5b2b06109b4e3f1f45b45dd00484c57da
AlfredoFi <theproductserial@hotmail.com>
2016-04-21 23:45:26 -0500

    Update EncodingClass.java

--- EncodingClass.java
+++ EncodingClass.java
@@ -2,25 +2,25 @@ public class EncodingClass {
   private static byte    byte_value = 101;
   private static short  short_value = 5;
   private static int      int_value = 100000001;
-  private static long    long_value = 123456787654321L;
+  private static long    long_value = 123456787654321L;   
 
   final static int byte_size = Byte.SIZE / Byte.SIZE;
   final static int int_size = Integer.SIZE / Byte.SIZE;
   final static int short_size = Short.SIZE / Byte.SIZE;
   final static int long_size = Long.SIZE / Byte.SIZE;
 
-  private final static int mask = 0xFF;
+  private final static int mask = 0xFF;  //mascara utilizada para el encodig
 
-  public static void printByteArray(byte[] val){
-    StringBuilder str = new StringBuilder();
-    for(byte b : val){
+  public static void printByteArray(byte[] val){  //función para imprimir por byte todo el arreglo
+    StringBuilder str = new StringBuilder(); //Decllaración de objeto str tipo Stringbuilder
+    for(byte b : val){   //aplica la mascara a cada byte
       str.append(b & mask).append(" ");
     }
     System.out.println(str.toString());
   }
 
   public static byte[] encodeBigEndian(long val, int size){
-    byte[] ret = new byte[size];
+    byte[] ret = new byte[size];  
     for(int i = 0;  i < size; i++){
       ret[i] =  (byte) (val >> (size - i - 1) * Byte.SIZE);
     }
@@ -41,7 +41,7 @@ public class EncodingClass {
   public static void main(String[] args){
     System.out.println("byte:" + byte_size + " short:" + 
         short_size + " int:" + int_size + " long:" + long_size);
-    byte[] byte2  = encodeBigEndian(345, short_size);
+    byte[] byte2  = encodeBigEndian(345, short_size);  
     byte[] byte1  = encodeBigEndian(84, byte_size);
     byte[] byte3  = catByteArr(byte2, byte1);
     byte[] byte8  = encodeBigEndian(1234567654321L, long_size);
